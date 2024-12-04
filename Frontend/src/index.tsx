3e317ecd78f9ed62a32afc78ffb3dea08bc8d0a4
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-04 12:55:57 +0000

    Initialise React Router

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './redux/store'
+import { CssBaseline } from '@mui/material'
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: import.meta.env.BASE_URL,
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router}/>
+      <CssBaseline/>
     </React.StrictMode>
   </Provider>
 )
