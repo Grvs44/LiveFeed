aca7d3af067e10ce3dbc6977899b83d48fee1641
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-05 21:25:30 +0000

    Use shaka-player-react https://github.com/matvp91/shaka-player-react

--- Frontend/src/components/Player.tsx
+++ Frontend/src/components/Player.tsx
@@ -1,34 +1,10 @@
 import React from 'react'
-import shaka from 'shaka-player'
+import ShakaPlayer from 'shaka-player-react'
+import 'shaka-player/dist/controls.css'
 
-// Adapted from https://dev.to/vanyaxk/shaka-player-for-media-playback-implementation-use-cases-pros-and-cons-3b87
 const exampleManifestUri =
   'https://storage.googleapis.com/livefeed-bucket/output/manifest.m3u8'
 
-async function initPlayer() {
-  // Install polyfills to patch out browser incompatibilities
-  shaka.polyfill.installAll()
-  // Check if your browser supports Shaka at all
-  if (!shaka.Player.isBrowserSupported()) {
-    console.error('Browser not supported!')
-    return
-  }
-
-  //Search for available video element and attach the player to it
-  const player = new shaka.Player(document.getElementById('video'))
-  // listen for errors for further error handling
-  player.addEventListener('error', console.error)
-  try {
-    await player.load(exampleManifestUri)
-    console.log('player has loaded the video, you can play it now')
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-// TODO: run from Player component
-document.addEventListener('DOMContentLoaded', initPlayer)
-
 export default function Player() {
-  return <p>Player</p>
+  return <ShakaPlayer autoPlay src={exampleManifestUri} />
 }
