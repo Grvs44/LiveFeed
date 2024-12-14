6e3fad58d2ad7415ec09dae5329299e87b8d863b
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-14 18:12:16 +0000

    Added initial chat feature

--- Frontend/src/containers/ChatContainer.tsx
+++
@@ -1,6 +0,0 @@
-import Box from "@mui/material/Box";
-import React from "react";
-
-export default function ChatContainer(){
-  return <Box>Chat container</Box>
-}
--- Frontend/src/redux/types.ts
+++ Frontend/src/redux/types.ts
@@ -2,6 +2,18 @@ export type TitleState = {
   title: string
 }
 
+export type Chat = {
+  id:number
+  username: string
+  message: string
+  time: string
+}
+
+export type ChatState = {
+  chats: Chat[]
+}
+
 export type State = {
   title: TitleState
+  chat: ChatState
 }
