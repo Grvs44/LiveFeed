a3df3bab1335cedcec0e72d9bbf5203a8a87fbb7
Mahdi Chowdhury <mc5u22@soton.ac.uk>
2024-12-16 00:22:21 +0000

    Stream start function started

--- Backend/function_app.py
+++ Backend/function_app.py
@@ -1,6 +1,31 @@
 import azure.functions as func
+import os
+import logging
 import datetime
 import json
 from shared_code import streaming
 
+app = func.FunctionApp()
+
 print(streaming.get_channel('livefeed-443712', 'europe-west2', 'livefeed-test-channel'))
+
+@app.route(route="stream/start", auth_level=func.AuthLevel.FUNCTION, methods=[func.HttpMethod.POST])
+def start_stream(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
