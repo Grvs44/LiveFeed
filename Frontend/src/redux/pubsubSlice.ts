bb2d091ad31c34afdbe770a55c441c5400b8a0f4
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-16 22:47:23 +0000

    Fixed client not being closed when provider is unmounted

---
+++ Frontend/src/redux/pubsubSlice.ts
@@ -0,0 +1,24 @@
+// Adapted from https://github.com/Grvs44/budgetmanager/blob/main/budgetmanagerpwa/src/redux/titleSlice.ts
+import { createSlice } from '@reduxjs/toolkit'
+import { PubsubState } from './types'
+
+const initialState: PubsubState = {}
+
+export const pubsubSlice = createSlice({
+  name: 'pubsub',
+  initialState,
+  reducers: {
+    setClient(state, action) {
+      state.client?.stop()
+      state.client = action.payload
+    },
+    resetClient(state) {
+      state.client?.stop()
+      state.client = undefined
+    },
+  },
+})
+
+export const { setClient, resetClient } = pubsubSlice.actions
+
+export default pubsubSlice.reducer
--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './apiSlice'
+import pubsubReducer from './pubsubSlice'
 import titleReducer from './titleSlice'
 
 export default configureStore({
   reducer: {
     title: titleReducer,
+    pubsub: pubsubReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
