4f8f7da5541c77deda7c60a4e449812cfb200d38
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-19 17:21:41 +0000

    Remove duplicated prop attributes

--- Frontend/src/components/VideoPlayer.tsx
+++ Frontend/src/components/VideoPlayer.tsx
@@ -2,14 +2,8 @@ import React from 'react'
 import ShakaPlayer from 'shaka-player-react'
 import 'shaka-player/dist/controls.css'
 
-export type VideoPlayerProps = React.VideoHTMLAttributes<HTMLVideoElement> & {
-  src?: string
-  autoPlay?: boolean
-  width?: number
-  height?: number
-}
+export type VideoPlayerProps = React.VideoHTMLAttributes<HTMLVideoElement>
 
 export default function VideoPlayer(props: VideoPlayerProps) {
-  console.log(props)
-  return <ShakaPlayer onTimeUpdate={props.onTimeUpdate} {...props} />
+  return <ShakaPlayer {...props} />
 }
