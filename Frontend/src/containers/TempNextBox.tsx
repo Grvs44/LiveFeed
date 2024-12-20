6c65e4db4db4d76cde2e9690ecfc55cb4d50da81
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-20 08:58:23 +0000

    Initialise current step ID to 1

--- Frontend/src/containers/TempNextBox.tsx
+++ Frontend/src/containers/TempNextBox.tsx
@@ -3,13 +3,21 @@ import React from 'react'
 import { PubSubClientContext } from '../context/PubSubClientProvider'
 
 export default function TempNextBox() {
-  const { currentStep, changeStep } = React.useContext(PubSubClientContext)
+  const { currentStep, changeStep, ready } =
+    React.useContext(PubSubClientContext)
+
+  React.useEffect(() => {
+    if (ready && changeStep) changeStep(1)
+  }, [ready])
+
   const onPrevious = () => {
     if (changeStep) changeStep((currentStep || 0) - 1)
   }
+
   const onNext = () => {
     if (changeStep) changeStep((currentStep || 0) + 1)
   }
+
   return (
     <div>
       <p>{currentStep}</p>
