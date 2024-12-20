5c5460f8ec8d5dd4479b4aebd7067f77cf86949c
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-12-20 18:01:13 +0000

    Handle start stream

--- Frontend/src/pages/RecipesPage.tsx
+++ Frontend/src/pages/RecipesPage.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import { Button, Container } from '@mui/material'
 import { useDispatch } from 'react-redux'
+import { useStartStreamMutation } from '../redux/apiSlice'
 import { setTitle } from '../redux/titleSlice'
 
 export default function RecipesPage() {
   const dispatch = useDispatch()
-  const { handleStart } = React.useContext();
+  const [startStream] = useStartStreamMutation()
+  const handleStart = async () => {
+    const stream = await startStream({ recipeName: 'hello' }).unwrap()
+    console.log('started stream:')
+    console.log(stream)
+  }
 
   React.useEffect(() => {
     dispatch(setTitle('Recipes'))
   }, [])
-  
-  return <>
-    <p>Recipe Manager</p>
-    <Button
-    onClick={handleStart}
-    
-  </>
+
+  return (
+    <Container>
+      <p>Recipe Manager</p>
+      <Button onClick={handleStart}>Start</Button>
+    </Container>
+  )
 }
