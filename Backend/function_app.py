e319a1e81c70ae1e6cf4facaa5be1ff80e81cd72
Setheze <160635339+Setheze@users.noreply.github.com>
2024-12-22 18:10:16 +0800

    Added steps and date

--- Backend/function_app.py
+++ Backend/function_app.py
@@ -1,4 +1,5 @@
 import azure.functions as func
+from azure.cosmos import CosmosClient
 import os
 import logging
 import datetime
@@ -13,6 +14,10 @@ app = func.FunctionApp()
 hub_name = 'livefeed'
 connection_string = os.environ.get('WebPubSubConnectionString')
 service = WebPubSubServiceClient.from_connection_string(connection_string, hub=hub_name)
+
+client = CosmosClient("https://livefeed-storage.documents.azure.com:443/", "RMcJvdRXCSCk60vX8ga7uAdnfl2yKW1nGBDf0EKcHc8NtdwKs72NAq2mDtUk8hW6NWwN3RnXMUFxACDbWLE70A==")
+database= client.get_database_client('Recipes')
+container = database.get_container_client('UploadedRecipes')
 
 #print(streaming.get_channel('livefeed-443712', 'europe-west2', 'livefeed-test-channel'))
 
@@ -36,6 +41,24 @@ def chat_negotiate(req: func.HttpRequest) -> func.HttpResponse:
     response_body = json.dumps({'url': token['url']})
     logging.info('Successful chat negotiation')
     return func.HttpResponse(response_body, status_code=200)
+
+@app.route(route="recipe/upload", auth_level=func.AuthLevel.FUNCTION, methods=[func.HttpMethod.POST])
+def upload_recipe(req: func.HttpRequest) -> func.HttpResponse:
+    info = req.get_json()
+    title = info.get('title')
+    steps = info.get('steps')
+    shoppingList = info.get('shoppingList')
+    date = info.get('scheduledDate')
+    
+    recipes = {
+        "title": title,
+        "steps": steps,
+        "shoppingList": shoppingList,
+        "date": date
+    }
+    
+    container.create_item(body=recipes, enable_automatic_id_generation=True)
+    return func.HttpResponse(json.dumps({"recipe_created": "OK"}), status_code=201, mimetype="application/json")
 
 
 # TODO: replace mock with real API
