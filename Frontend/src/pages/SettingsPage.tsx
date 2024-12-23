dcd002bfead17125c338abf4dcd6a6354898b2e5
Ruqayah <rr5g22@soton.ac.uk>
2024-12-23 03:01:11 +0000

    Non-functional radio group added for email notifications on/off

--- Frontend/src/pages/SettingsPage.tsx
+++ Frontend/src/pages/SettingsPage.tsx
@@ -10,16 +10,25 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import UserUpdateForm from '../components/UserUpdateForm';
 import TagsChipArray from '../components/TagsChipArray';
+import FormControl from '@mui/material/FormControl';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
 
 
 export default function SettingsPage() {
   const dispatch = useDispatch()
-  const { activeAccount } = React.useContext(LoginContext);
+  const { activeAccount } = React.useContext(LoginContext)
   const [userDetails] = React.useState<UserInfo>({
     name: (activeAccount?.idTokenClaims?.name as string) ?? '',
     given_name: (activeAccount?.idTokenClaims?.given_name as string) ?? '',
     family_name: (activeAccount?.idTokenClaims?.family_name as string) ?? '',
-  });
+  })
+  const [notifications, setNotifications] = React.useState('yes')
+
+  const handleNotificationChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setNotifications(event.target.value)
+  };
 
   React.useEffect(() => {
     dispatch(setTitle('Settings'))
@@ -32,17 +41,25 @@ export default function SettingsPage() {
     </Typography>
     <UserUpdateForm/>
     <Divider />
-    
     <TagsChipArray/>
-    
     <Divider />
 
     <Box sx={{ marginTop: '40px' }}>
-      <Typography variant="h6" sx={{ fontSize: '1.125rem' }} gutterBottom>
-        Notifications
-      </Typography>
       <Box sx={{ padding: '20px', border: '1px solid #ddd', borderRadius: '8px' }}>
-        {/* Need to add content */}
+      <FormControl component="fieldset">
+            <Typography variant="h6" sx={{ fontSize: '1.125rem' }} gutterBottom>
+              Email Notifications
+            </Typography>
+            <RadioGroup
+              aria-label="notifications"
+              name="notifications"
+              value={notifications}
+              onChange={handleNotificationChange}
+            >
+              <FormControlLabel value="yes" control={<Radio/>} label="Yes, send me notifications" />
+              <FormControlLabel value="no" control={<Radio/>} label="No, I don't want notifications" />
+            </RadioGroup>
+          </FormControl>
       </Box>
     </Box>
   </Box>
