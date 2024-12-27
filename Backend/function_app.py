1c0ce4cdd60131276e3781edd251fff0bb9c3a9f
Mahdi Chowdhury <mc5u22@soton.ac.uk>
2024-12-27 16:43:50 +0000

    Added proper stream/vod deletion to recipe deletion

--- Backend/function_app.py
+++ Backend/function_app.py
@@ -380,6 +380,8 @@ def delete_recipe(req: func.HttpRequest) -> func.HttpResponse:
         
 
         recipe_container.delete_item(item=id,partition_key=user_id)
+        streaming.delete_vod(id)
+        stream_container.delete_item(id, partition_key=id)
         return func.HttpResponse(json.dumps({"recipe_updated": "OK"}), status_code=200, mimetype="application/json")
 
     except Exception as e:
