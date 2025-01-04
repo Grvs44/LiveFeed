d82cb92cd610d826b76526423d1ab85216673bfb
Ruqayah <rr5g22@soton.ac.uk>
2025-01-04 14:56:23 +0000

    Created pre-set user tags

---
+++ Frontend/src/config/Tags.ts
@@ -0,0 +1,31 @@
+export interface Tag {
+    id: string
+    name: string
+  }
+
+const TAGS: Tag[] = [
+  { id: '1', name: 'Vegetarian' },
+  { id: '2', name: 'Vegan' },
+  { id: '3', name: 'Gluten Free' },
+  { id: '4', name: 'Dairy Free' },
+  { id: '5', name: 'Nut Free' },
+  { id: '6', name: 'Low Carb' },
+  { id: '7', name: 'High Protein' },
+  { id: '8', name: 'Meal Prep' },
+  { id: '9', name: 'Air Fryer' },
+  { id: '10', name: 'Beginner Friendly' },
+  { id: '11', name: 'Dessert' },
+  { id: '12', name: 'Healthy' },
+  { id: '13', name: 'Comfort Food' },
+  { id: '14', name: 'Spicy' },
+  { id: '15', name: 'Breakfast' },
+  { id: '16', name: 'Lunch' },
+  { id: '17', name: 'Dinner' },
+  { id: '18', name: 'Snacks' },
+  { id: '19', name: 'One-Pot Meals' },
+  { id: '20', name: 'Party Food' },
+  { id: '21', name: 'BBQ'},
+  { id: '22', name: 'Low Budget'},
+]
+
+export default TAGS
