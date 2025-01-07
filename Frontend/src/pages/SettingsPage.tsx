a63865ffecdf77211f293acf24960025ee1510a0
Ruqayah <rr5g22@soton.ac.uk>
2025-01-07 14:21:17 +0000

    Modified settings page to use Redux

--- Frontend/src/pages/SettingsPage.tsx
+++ Frontend/src/pages/SettingsPage.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import Typography from '@mui/material/Typography'
+import { useDispatch, useSelector } from 'react-redux'
+import TagsChipArray from '../components/TagsChipArray'
+import UserUpdateForm from '../components/UserUpdateForm'
+import { LoginContext } from '../context/LoginProvider'
+import { UserInfo } from '../context/types'
+import { setNotif } from '../redux/notifSlice'
 import { setTitle } from '../redux/titleSlice'
-import { LoginContext } from '../context/LoginProvider';
-import { UserInfo } from '../context/types';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import UserUpdateForm from '../components/UserUpdateForm';
-import TagsChipArray from '../components/TagsChipArray';
-import FormControl from '@mui/material/FormControl';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Radio from '@mui/material/Radio';
-
+import { State } from '../redux/types'
 
 export default function SettingsPage() {
   const dispatch = useDispatch()
@@ -24,44 +23,63 @@ export default function SettingsPage() {
     given_name: (activeAccount?.idTokenClaims?.given_name as string) ?? '',
     family_name: (activeAccount?.idTokenClaims?.family_name as string) ?? '',
   })
-  const [notifications, setNotifications] = React.useState('yes')
+  const notificationsEnabled = useSelector(
+    (state: State) => state.notif.enabled,
+  )
 
-  const handleNotificationChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setNotifications(event.target.value)
-  };
+  const handleNotificationChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = event.target.value === 'yes'
+    dispatch(setNotif(value))
+  }
 
   React.useEffect(() => {
     dispatch(setTitle('Settings'))
   }, [])
 
-  
-  return (<Box sx={{ width: '100%', padding: '10px 30px 30px'  }}>
-    <Typography variant="h6" gutterBottom align="center" >
-      Manage your details and preferences
-    </Typography>
-    <UserUpdateForm/>
-    <Divider />
-    <TagsChipArray/>
-    <Divider />
+  return (
+    <Box sx={{ width: '100%', padding: '10px 30px 30px' }}>
+      <Typography variant="h6" gutterBottom align="center">
+        Manage your details and preferences
+      </Typography>
+      <UserUpdateForm />
+      <Divider />
+      <TagsChipArray />
+      <Divider />
 
-    <Box sx={{ marginTop: '40px' }}>
-      <Box sx={{ padding: '20px', border: '1px solid #ddd', borderRadius: '8px' }}>
-      <FormControl component="fieldset">
+      <Box sx={{ marginTop: '40px' }}>
+        <Box
+          sx={{
+            padding: '20px',
+            border: '1px solid #ddd',
+            borderRadius: '8px',
+          }}
+        >
+          <FormControl component="fieldset">
             <Typography variant="h6" sx={{ fontSize: '1.125rem' }} gutterBottom>
               Email Notifications
             </Typography>
             <RadioGroup
               aria-label="notifications"
               name="notifications"
-              value={notifications}
+              value={notificationsEnabled}
               onChange={handleNotificationChange}
             >
-              <FormControlLabel value="yes" control={<Radio/>} label="Yes, send me notifications" />
-              <FormControlLabel value="no" control={<Radio/>} label="No, I don't want notifications" />
+              <FormControlLabel
+                value="yes"
+                control={<Radio />}
+                label="Yes, send me notifications"
+              />
+              <FormControlLabel
+                value="no"
+                control={<Radio />}
+                label="No, I don't want notifications"
+              />
             </RadioGroup>
           </FormControl>
+        </Box>
       </Box>
     </Box>
-  </Box>
-  );
+  )
 }
