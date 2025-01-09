123d2b2a0c1ed31f4846aadd6e49754350c08a6e
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-01-09 08:48:21 +0000

    Change recipe step with video time

--- Frontend/src/components/VideoPlayer.tsx
+++ Frontend/src/components/VideoPlayer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ShakaPlayer from 'shaka-player-react'
 import 'shaka-player/dist/controls.css'
 
-export type VideoPlayerProps = React.VideoHTMLAttributes<HTMLVideoElement>
+export type VideoPlayerProps = React.VideoHTMLAttributes<HTMLVideoElement> & {
+  ref?: React.MutableRefObject<HTMLVideoElement | null>
+}
 
 export default function VideoPlayer(props: VideoPlayerProps) {
   return <ShakaPlayer {...props} />
